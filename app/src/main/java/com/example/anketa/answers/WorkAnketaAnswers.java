54a01f392b4618bf72967a0c1dc05318e238ae71
daryaditte <hatorubi90@gmail.com>
2018-11-22 23:54:53 +0500

    Статистика РАБОТА

--- app/src/main/java/com/example/anketa/answers/WorkAnketaAnswers.java
+++ app/src/main/java/com/example/anketa/answers/WorkAnketaAnswers.java
@@ -2,7 +2,7 @@ package com.example.anketa.answers;
 
 import com.github.mikephil.charting.data.BarEntry;
 
-import java.util.List;
+import java.util.*;
 
 
 
@@ -22,27 +22,96 @@ public class WorkAnketaAnswers extends Anketa{
     public String eighth;
 
 
+    private static final Map<Integer, String> queryTitles = new HashMap<>();
+
+    static {
+        queryTitles.put(1,  "Вопрос 1");
+        queryTitles.put(2,  "Вопрос 2");
+        queryTitles.put(3,  "Вопрос 3");
+        queryTitles.put(4,  "Вопрос 5");
+        queryTitles.put(5,  "Вопрос 6");
+        queryTitles.put(6,  "Вопрос 7");
+    }
+
+
+    private static final Map<Integer, List<String>> queryVarian = new HashMap<>();
+
+    static {
+        queryVarian.put(1, Arrays.asList("Менее 18-и", "18-24", "25-44", "45-64", "64 и выше"));
+        queryVarian.put(2, Arrays.asList("Работающий/ая на полную ставку", "Работающий/ая неполный рабочий день", "Безработный/ая", "Частный предпринематель", "Студент", "Пенсионер"));
+        queryVarian.put(3, Arrays.asList("Начальное образование", "Неполное среднее образование", "Среднеспециальное образование", "Высшее профессиональное образование", "Отсутствует"));
+        queryVarian.put(4, Arrays.asList("Государственный сектор", "Частный сектор", "Сам/а для себя"));
+        queryVarian.put(5, Arrays.asList("На улице, на природе", "Предпочитаю тихий офис", "В шумном месте, где много интересных людей", "В лаборатории", "В классе"));
+        queryVarian.put(6, Arrays.asList("Людьми", "Вещами", "Информацией", "Деньгами"));
+    }
+
+
     public List<String> getAnswerChoices(int queryNum) {
-        return null;
+        return queryVarian.get(queryNum);
     }
 
 
-    public List<Object> getAnswer(int queryNum) {
-        return null;
+    @Override
+    public List<BarEntry> getAnswer(int queryNum, List<Anketa> data) {
+        List<BarEntry> result = new ArrayList<>();
+
+
+        Map<String, Integer> statistic = new HashMap<>();
+        for (String answer : getAnswerChoices(queryNum)) {
+            statistic.put(answer, 0);
+        }
+
+        int other = 0;
+        for (Anketa anketa : data) {
+            WorkAnketaAnswers answer = (WorkAnketaAnswers) anketa;
+            switch (queryNum) {
+                case 1:
+                    increment(statistic, answer.first);
+                    break;
+                case 2:
+                    increment(statistic, answer.second);
+                    break;
+                case 3:
+                    increment(statistic, answer.third);
+                    break;
+                case 4:
+                    increment(statistic, answer.fifth);
+                    break;
+                case 6:
+                    increment(statistic, answer.seventh);
+                    break;
+
+                case 5:
+                    if (statistic.containsKey(answer.sixth)) {
+                        increment(statistic, answer.sixth);
+                    } else {
+                        other++;
+                    }
+                    break;
+            }
+        }
+
+        for (int i = 0; i < getAnswerChoices(queryNum).size(); i++) {
+            String key = getAnswerChoices(queryNum).get(i);
+            result.add(new BarEntry(i, statistic.get(key), key));
+        }
+
+        if (queryNum == 5) {
+            result.add(new BarEntry(getAnswerChoices(queryNum).size(), other, "В другом месте"));
+        }
+
+        return result;
     }
 
 
-    @Override public List<BarEntry> getAnswer(int queryNum, List<Anketa> data) {
-        return null;
+    @Override
+    public int getAnswerCount() {
+        return queryTitles.size();
     }
 
 
-    @Override public int getAnswerCount() {
-        return 0;
-    }
-
-
-    @Override public String getQueryTitle(int queryNum) {
-        return null;
+    @Override
+    public String getQueryTitle(int queryNum) {
+        return queryTitles.get(queryNum);
     }
 }
